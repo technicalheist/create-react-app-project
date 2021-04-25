b010a9dd689ec808362c4f73f9d82b87d6013dd1
Technical Heist <contact@technicalheist.com>
2021-04-26 02:49:24 +0530

    First Stable Version Added

--- post_install.js
+++ post_install.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var gentlyCopy = require('gently-copy')
-
+var fs = require('fs');
 var filesToCopy = ['public', 'src', '.gitignore', 'LICENSE', 'package.json', 'README.md', 'tsconfig.json', 'yarn.lock'];
 
 // User's local directory
@@ -9,3 +9,8 @@ var userPath = process.env.INIT_CWD
 
 // Moving files to user's local directory
 gentlyCopy(filesToCopy, userPath)
+
+fs.writeFile('../../post_install.js', "console.log('To start the app run npm start or yarn start Happy Coding')", function (err, file) {
+    if (err) throw err;
+    console.log('Installation Completed');
+  }); 
