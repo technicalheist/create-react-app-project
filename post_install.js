a1e4f9d356401b92c9266d7f5229c3d168204204
Shivrajan <shivrajansingh@gmail.com>
2023-01-11 21:08:50 +0530

    Version CHanged

--- post_install.js
+++ post_install.js
@@ -1,16 +1,19 @@
-'use strict'
-
-var gentlyCopy = require('gently-copy')
 var fs = require('fs');
-var filesToCopy = ['public', 'src', '.gitignore', 'LICENSE', 'package.json', 'README.md', 'tsconfig.json', 'yarn.lock'];
+const { spawn } = require("child_process");
+var installCopy = spawn("npm", ["i", "gently-copy"]);
+installCopy.on('exit', (code)=>{
+    var gentlyCopy = require('gently-copy')
+    var filesToCopy = ['public', 'src', '.gitignore', 'LICENSE', 'package.json', 'README.md', 'tsconfig.json', 'yarn.lock'];
+    // User's local directory
+    var userPath = process.env.INIT_CWD
+    // Moving files to user's local directory
+    gentlyCopy(filesToCopy, userPath);
+    console.log('All done');
+})
 
-// User's local directory
-var userPath = process.env.INIT_CWD
 
-// Moving files to user's local directory
-gentlyCopy(filesToCopy, userPath)
 
-fs.writeFile('../../post_install.js', "console.log('To start the app run npm start or yarn start Happy Coding')", function (err, file) {
-    if (err) throw err;
-    console.log('Installation Completed');
-  }); 
+
+
+
+
